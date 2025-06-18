9c06644756871a5a9058861fa1b5299c6e64efdf
MonsterousDev <tony.awesome.dev@gmail.com>
2025-06-18 00:33:21 -0500

    feat: Placed fillout Logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header className="w-full flex flex-col items-center justify-center py-8 bg-white shadow-sm mb-8">
+        <img src="/images.png" alt="Fillout Logo" className="w-60 h-20 mb-3 shadow" />
+      </header>
+      <main className="max-w-4xl mx-auto px-4">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { StepNavigation } from "@/features/navigation/components/StepNavigation"
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center py-12">
+    <main className="flex flex-col min-h-screen items-center py-12">
       <StepNavigation />
     </main>
   );
